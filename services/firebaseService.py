08d3ccc4198622db0530bb528aef52e3863d69af
eliasstein <eliasstein@users.noreply.github.com>
2025-02-12 19:50:08 -0300

    Segunda prueba usando variables de entorno.

--- services/firebaseService.py
+++ services/firebaseService.py
@@ -2,15 +2,15 @@ import pyrebase
 import os,json
 from dotenv import dotenv_values
 
-secret=dotenv_values(".env")
+secret=dotenv_values(".env")["FIREBASE_CONFIG"]
 
 fb_config = {
-    "apiKey": secret["APIKEY"], #El [1:-2] es para borrar las comillas y el salto de linea
-    "authDomain": f"{secret["PROJECT_ID"]}.firebaseapp.com",
-    "databaseURL":f"https://{secret["PROJECT_ID"]}-default-rtdb.firebaseio.com",
-    "projectId": f"{secret["PROJECT_ID"]}",
-    "storageBucket": f"{secret["PROJECT_ID"]}.appspot.com",
-    "serviceAccount":json.loads(secret["FIREBASE_CONFIG"])
+    "apiKey": os.getenv("APIKEY"), #El [1:-2] es para borrar las comillas y el salto de linea
+    "authDomain": f"{os.getenv("PROJECT_ID")}.firebaseapp.com",
+    "databaseURL":f"https://{os.getenv("PROJECT_ID")}-default-rtdb.firebaseio.com",
+    "projectId": f"{os.getenv("PROJECT_ID")}",
+    "storageBucket": f"{os.getenv("PROJECT_ID")}.appspot.com",
+    "serviceAccount":json.loads(secret)
 }
 print(secret)
 fb=pyrebase.initialize_app(fb_config)
