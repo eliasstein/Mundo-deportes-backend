6938b109b721c2b701541e5f79c520a5f68e1c16
eliasstein <eliasstein@users.noreply.github.com>
2025-02-13 17:05:22 -0300

    Update user.py

--- routes/user.py
+++ routes/user.py
@@ -36,18 +36,15 @@ def read_root(user_login:usermodel.login):
 
     resp.headers.append("set-cookie",f"usrnm={usrdata["data"]["username"]};"+
                                            "Max-Age=3600;"+
-                                           "Path=/;"+
-                                           "SameSite=None;")
+                                           "Path=/;")
     resp.headers.append("set-cookie",f"localId={user["localId"]};"+
                                            "Max-Age=3600;"+
-                                           "Path=/;"+
-                                           "SameSite=None;")
+                                           "Path=/;")
 
 
     resp.headers.append("set-cookie",f"idToken={user["idToken"]};"+
                                            f"Max-Age=3600;"+
-                                           f"Path=/;"+
-                                           f"SameSite=None;")
+                                           f"Path=/;")
 
     
     # resp.set_cookie(key="usrnm",value=usrdata["data"]["username"], max_age=3600,secure=True,samesite="None")
@@ -55,8 +52,7 @@ def read_root(user_login:usermodel.login):
     # resp.set_cookie(key="idToken",value=user["idToken"], max_age=3600,secure=True,samesite="None")
     if user_login.remember:
         resp.headers.append("set-cookie",f"refreshToken={user["refreshToken"]};"+
-                                        f"Path=/;"+
-                                        f"SameSite=None;")
+                                        f"Path=/;")
 
         # resp.set_cookie(key="refreshToken",value=user["refreshToken"],secure=True,samesite="None")
     return resp
